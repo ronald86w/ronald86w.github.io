84b3efbac1edc523eb1c9a05513e571762d9a0a4
ronald86w <54478526+ronald86w@users.noreply.github.com>
2021-05-02 19:49:59 -0500

    haberdashers

--- assets/custom/gallery.js
+++ assets/custom/gallery.js
@@ -1,6 +1,20 @@
 function myFunction(imgs) {
   var expandImg = document.getElementById("expandedImg");
   expandImg.src = imgs.src;
-
   expandImg.parentElement.style.display = "block";
+}
+function myFunction2(imgs) {
+  var expandImg2 = document.getElementById("expandedImg2");
+  expandImg2.src = imgs.src;
+  expandImg2.parentElement.style.display = "block";
+}
+function myFunction3(imgs) {
+  var expandImg3 = document.getElementById("expandedImg3");
+  expandImg3.src = imgs.src;
+  expandImg3.parentElement.style.display = "block";
+}
+function myFunction4(imgs) {
+  var expandImg4 = document.getElementById("expandedImg4");
+  expandImg4.src = imgs.src;
+  expandImg4.parentElement.style.display = "block";
 }
